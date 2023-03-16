7479329a19729e490c02e915be936fe3c7da7fc1
Harry Tu <tuchenhsien@gmail.com>
2023-03-16 17:58:42 +0800

    complete flask backend

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, make_response, request
+from flask_restful import Api, Resource
+import tensorflow as tf
+import pandas as pd
+import prediction
+from flask_cors import CORS
+# import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import MinMaxScaler
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+@app.route('/get-rental-prediction-price', methods=['GET'])
+def get_rental_prediction_price():
+    bedroom = request.args.get("bedroom")
+    bathroom = request.args.get("bathroom")
+    den = request.args.get("den")
+    lat = request.args.get("lat")
+    long = request.args.get("long")
+    result = prediction.transformModel([bedroom, bathroom, den, lat, long])
+    return jsonify(str(result))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ prediction.py
@@ -0,0 +1,36 @@
+from flask_restful import Api, Resource
+import tensorflow as tf
+import pandas as pd
+# import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import MinMaxScaler
+
+
+def transformModel(params):
+    path_to_model = "./assets/model/toronto_rental_prediction_v2.h5"
+    path_to_csv = "./assets/model/Toronto_apartment_rentals_2018.csv"
+    model = tf.keras.models.load_model(path_to_model)
+    rental = pd.read_csv(path_to_csv)    
+    # Create X & y
+    X = rental.drop(["Price", "Address"], axis=1)
+    y = rental["Price"]
+    # data = [2, 1, 0, 43.648861, -79.378033]
+    data = params
+    columns = ["Bedroom", "Bathroom", "Den", "Lat", "Long"]
+    df = pd.DataFrame([data], columns=columns)
+    # Create column transformer (this will help us normalize/preprocess our data)
+    ct = make_column_transformer(
+        (MinMaxScaler(), columns), # get all values between 0 and 1
+    )
+
+    # Build our train and test sets (use random state to ensure same split as before)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+    # Fit column transformer on the training data only (doing so on test data would result in data leakage)
+    ct.fit(X_train)
+
+    # Transform training and test data with normalization (MinMaxScalar) and one hot encoding (OneHotEncoder)
+    X_test_normal = ct.transform(df)
+    result = model.predict(X_test_normal)        
+    return result
