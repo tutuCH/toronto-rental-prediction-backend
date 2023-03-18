4fbb2365730464bfc8f745148bb65597680f2e92
Harry Tu <tuchenhsien@gmail.com>
2023-03-18 17:50:24 +0800

    update error handling

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from flask import Flask, jsonify, make_response, request
 from flask_restful import Api, Resource
 import prediction
 from flask_cors import CORS
+import urllib.parse
+import os
+from dotenv import load_dotenv;
 
+load_dotenv()
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
@@ -17,12 +21,18 @@ def get_rental_prediction_price():
     bathroom = request.args.get("bathroom")
     den = request.args.get("den")
     address = request.args.get("address")
-    coordinates = prediction.getLatLongByAddress(address)
-    if (coordinates == "ADDRESS_NOT_IN_TORONTO"):
-        return coordinates
+    try:
+        coordinates = prediction.getLatLongByAddress(address)
+        if (coordinates == "ADDRESS_NOT_IN_TORONTO"):
+            return coordinates
+    except:
+        return "getLatLongByAddress error, address: "+address+"url: "+os.getenv('GEOAPIFY_BASE_URL') + urllib.parse.quote(address, safe='') + "&apiKey=" + os.getenv('GEOAPIFY_API_KEY')
     else:
-        result = prediction.transformModel([bedroom, bathroom, den, coordinates[0], coordinates[1]])
-        return jsonify(str(result))
+        try:
+            result = prediction.transformModel([bedroom, bathroom, den, coordinates[0], coordinates[1]])
+            return jsonify(str(result))
+        except:
+            return "transformModel error"
 
 if __name__ == "__main__":
     app.run(debug=True)
